aa009acf5ef107b5ada8a8359a4670922d87e456
Emily G <aspentree79@gmail.com>
2019-08-22 01:50:14 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -10,16 +10,20 @@ end
 
 
 
-def find_max_value(array)
-  max = 0
-    array.length.times do |count|
-      if array[count] > max
-        max = array[count]
-      end
-    end
-  max
-end
+# def find_max_value(array)
+#   max = 0
+#     array.length.times do |count|
+#       if array[count] > max
+#         max = array[count]
+#       end
+#     end
+#   max
+# end
 
+def find_max_value(array)
+  number = array.max
+  return number
+end
 
 def find_min_value(array)
 number = array.min
