4c2fac057c7e06ba7e15317e03518511853879cf
Emily G <aspentree79@gmail.com>
2019-08-22 01:39:43 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -21,9 +21,11 @@ def find_max_value(array)
 end
 
 
-# def find_min_value(array)
-# i = 0
-# while array.length.times do |number|
-#
-# end
-# end
+def find_min_value(array)
+min = 0
+array.length.times do |number|
+if array[number] == min
+  min = array[number]
+end
+min
+end
